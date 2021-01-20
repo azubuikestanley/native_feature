f10b2977418243f43e537517c2193828acdc5d31
A-Stanley <astanley@uplandsoftware.com>
2021-01-20 19:54:22 +0100

    storing the location in sqlite

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 
 String googleApiKey = DotEnv().env['KEY'];
 
@@ -8,5 +11,13 @@ class LocationHelper {
     double longitude,
   }) {
     return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:purple%7Clabel:A%7C$latitude,$longitude&key=$googleApiKey';
+  }
+
+  static Future<String> getPlaceAddress(double lat, double lng) async {
+    final url =
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$googleApiKey';
+    final response = await http.get(url);
+    print(response.body);
+    return json.decode(response.body)['results'][0]['formatted_address'];
   }
 }
